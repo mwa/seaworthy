8f7e064de6345a0ae3d0e1296726ea1b9561e558
Mathias Walker <mwa@sourcepole.ch>
2020-03-30 20:29:16 +0200

    add tween animation for fish pickup

--- scripts/Fish.gd
+++ scripts/Fish.gd
@@ -4,6 +4,23 @@ extends Area2D
 var speed = 150  # speed in pixels/s
 var velocity = Vector2(0, speed)
 
+var tween = null
+var tween_duration = 0.2
+
+
+func _ready():
+	# setup tween animation
+	tween = Tween.new()
+	tween.interpolate_property(
+		$Sprite, 'scale',
+		$Sprite.get_scale(), Vector2.ONE * 1.5, tween_duration,
+		Tween.TRANS_QUAD, Tween.EASE_OUT)
+	tween.interpolate_property(
+		$Sprite, 'modulate:a',
+		$Sprite.modulate.a, 0, tween_duration,
+		Tween.TRANS_QUAD, Tween.EASE_OUT)
+	add_child(tween)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,4 +44,8 @@ func _on_Fish_body_entered(body):
 	if body.has_method('on_fish_touched'):
 		# handle collision
 		body.on_fish_touched()
-		remove()
+
+		# animate
+		z_index = 2
+		tween.connect('tween_all_completed', self, 'remove')
+		tween.start()
